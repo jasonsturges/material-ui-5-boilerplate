418b9a0f7deb23ddddcd78b94212cd450757605d
Jason Sturges <jason@jasonsturges.com>
2021-07-16 11:35:32 -0500

    Styled engine provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import { CssBaseline, StyledEngineProvider } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
 import NotistackProvider from "./providers/NotistackProvider";
 import store from "./store";
 import theme from "./theme/theme";
@@ -9,12 +10,14 @@ import Routes from "./Routes";
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <NotistackProvider>
-          <Routes />
-        </NotistackProvider>
-      </ThemeProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <NotistackProvider>
+            <Routes />
+          </NotistackProvider>
+        </ThemeProvider>
+      </StyledEngineProvider>
     </Provider>
   );
 }
